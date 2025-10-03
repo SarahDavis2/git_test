a93249844ee4a593687f6432012cac28c9a61f84
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-10-03 13:02:15 -0500

    Additional resources practice questions

--- js/js.js
+++ js/js.js
@@ -25,33 +25,105 @@
 // div.appendChild(h1);
 // div.appendChild(para2);
 
-function Book(title, author, pages, read) {
-    if (!new.target) {
-        throw Error("ERROR! Use the new operator.");
+// function Book(title, author, pages, read) {
+//     if (!new.target) {
+//         throw Error("ERROR! Use the new operator.");
+//     }
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.info = function() {
+//         return `${title} by ${author}, ${pages} pages, ${read}`;
+//     };
+// }
+
+// const book = new Book("The Hobbit", "J.K.K Tolkien", 295, "not read yet");
+
+// const module = (() => {
+//     console.log("module says");
+
+//     const test = () => {
+//         console.log("test");
+//     }
+//     return {
+//         test,
+//     }
+// })();
+
+// const start = (() => {
+//     console.log("start");
+//     module.test();
+// })();
+
+// class Person {
+//     constructor(name, age, country) {
+//         this.name = name;
+//         this.age = age;
+//         this.country = country;
+//     }
+//     get details() {
+//         return `${this.name} is ${this.age} years old, and from ${this.country}`;
+//     }
+// }
+
+// p1 = new Person("Jerry", 21, "USA");
+// p2 = new Person("James", 17, "MEXICO");
+
+// class Rectangle {
+//     static #PERIMETER_MULTIPLIER = 2;
+
+//     constructor(width, height) {
+//         this.width = width;
+//         this.height = height;
+//     }
+//     get area() {
+//         return this.width * this.height;
+//     }
+//     get perimeter() {
+//         return (this.width + this.height) * Rectangle.#PERIMETER_MULTIPLIER;
+//     }
+// }
+
+// r1 = new Rectangle(2, 5);
+
+// class Vehicle {
+//     constructor(make, model, year) {
+//         this.make = make;
+//         this.model = model;
+//         this.year = year;
+//     }
+//     showDetails() {
+//         console.log(this.make);
+//         console.log(this.model);
+//         console.log(this.year);
+//     }
+// }
+
+// class Car extends Vehicle {
+//     constructor(make, model, year, doors) {
+//         super(make, model, year);
+//         this.doors = doors;
+//     }
+//     showDetails() {
+//         super.showDetails();
+//         console.log(this.doors);
+//     }
+// }
+
+// car = new Car("Honda", "v8", 2023, 5);
+
+class BankAccount {
+    #amt = null;
+    constructor(amt) {
+        this.#amt = amt;
     }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}`;
-    };
+    deposit(amt) {
+        this.#amt += amt;
+    }
+    withdraw(amt) {
+        this.#amt -= amt;
+    }
 }
 
-const book = new Book("The Hobbit", "J.K.K Tolkien", 295, "not read yet");
-
-const module = (() => {
-    console.log("module says");
-
-    const test = () => {
-        console.log("test");
-    }
-    return {
-        test,
-    }
-})();
-
-const start = (() => {
-    console.log("start");
-    module.test();
-})();
+b = new BankAccount(200);
